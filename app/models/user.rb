96cc308c7d5e8c5e8d0a050c4d08050a2f28d362
AlejandraKeber <alejandrakeber@gmail.com>
2023-07-14 13:50:56 -0500

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
   has_many :videogames, through: :reservations
 
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 50 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6, maximum: 20 }
   validates :address, presence: true, length: { minimum: 2, maximum: 255 }
   validates :admin, inclusion: { in: [true, false] }
