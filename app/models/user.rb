a13380e85a1a43174bca01ad97d02483cf670433
AlejandraKeber <alejandrakeber@gmail.com>
2023-07-14 13:49:10 -0500

    Add user  validations and asociations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
+
+  has_many :reservations
+  has_many :videogames, through: :reservations
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6, maximum: 20 }
+  validates :address, presence: true, length: { minimum: 2, maximum: 255 }
+  validates :admin, inclusion: { in: [true, false] }
 end
