4ecccc816e76a4798d3132cf3e950cddc2e6f2b9
AlejandraKeber <alejandrakeber@gmail.com>
2023-07-17 14:21:44 -0500

    Add endpoint for new reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,4 +5,32 @@ class ReservationsController < ApplicationController
     @reservations = Reservation.all
     render json: @reservations
   end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @videogame = Videogame.find(params[:reservation][:videogame_id])
+    days = params[:reservation][:days].to_i
+    total_price = @videogame.price_per_day * days
+
+    @reservation = current_user.reservations.build(
+      videogame_id: @videogame.id,
+      days: days,
+      total_price: total_price
+    )
+
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:videogame_id, :days, :total_price)
+  end
 end
