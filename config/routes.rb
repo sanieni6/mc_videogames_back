aecc2c5e65146e818ad84933bbcc3daa478c6f84
AlejandraKeber <alejandrakeber@gmail.com>
2023-07-19 18:06:18 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  root to: redirect('/api-docs')
+  
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  
   devise_for :users,
               controllers: {
                 sessions: 'users/sessions',
                 registrations: 'users/registrations'
               }
   get '/member-data', to: 'members#show'
-
 
   resources :reservations, only: [:index, :new, :create, :show]
   resources :videogames, only: [:index, :show, :new, :create, :destroy]
