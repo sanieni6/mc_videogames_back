74c3ba727e11e4073a02abba7c9496d5d3351a7b
sanieni6 <luissanchezzamora23@gmail.com>
2023-07-20 10:49:22 -0500

    Change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs', as: 'api_docs'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :users,
               controllers: {
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: redirect('/api-docs')
+
 end
