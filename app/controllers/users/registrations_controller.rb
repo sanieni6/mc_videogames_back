19a12e4f29e62977950589a40f313830d28a4175
sanieni6 <luissanchezzamora23@gmail.com>
2023-07-21 17:14:57 -0500

    Refactor code, add exceptions and fix bugs

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!, except: %i[create]
+
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      sign_in(resource)
+      register_success
+    else
+      register_failed(resource)
+    end
+  end
+
   private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :address)
+  end
 
   def respond_with(resource, _opts = {})
     register_success && return if resource.persisted?
@@ -15,6 +32,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def register_failed
-    render json: { message: 'Something went wrong.' }, status: :unprocessable_entity
+    render json: { message: 'Something went wrong.', errors: resource.errors.full_messages },
+           status: :unprocessable_entity
   end
 end
